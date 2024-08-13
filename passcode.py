53ffe22086ae02d454c9db17c6b508ab9803bd1d
0ld Camel <0ldcamel502@gmail.com>
2024-08-13 21:42:12 +0300

    update passcode.py

--- passcode.py
+++ passcode.py
@@ -10,8 +10,9 @@ def main():
     print('Number of possible permutations before conditions:',len(permutations))
     correct_number, correct_index = test_well_placed()
 
-    purge_cond1(correct_number, correct_index, permutations)
+    purge_cond1(correct_number, correct_index, permutations) # purging incorrect numbers in cond1
     print('Number of possible permutations after applying condition 1:',len(permutations))
+    
     wrongly_placed_list = [[cond2, 1], [cond3, 2], (cond4, 0), (cond5, 1)]
     i = 2
     for cond, quantity in wrongly_placed_list:
@@ -53,4 +54,5 @@ def test_wrongly_placed(permutations, cond, quantity):
 
     return permutations
 
-main()
+if __name__ == '__main__':
+    main()
