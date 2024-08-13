5f169cb8819296d0876e179a73acad44d87d3117
0ld Camel <0ldcamel502@gmail.com>
2024-08-13 21:40:05 +0300

    update passcode.py

--- passcode.py
+++ passcode.py
@@ -7,26 +7,22 @@ cond5 = [7, 8, 0] # 1 number wrongly placed
 
 def main():
     permutations = [list(permu) for permu in itertools.permutations(list(range(10)), 3)]
-    print('Number of possible permutations before purging     :',len(permutations))
+    print('Number of possible permutations before conditions:',len(permutations))
     correct_number, correct_index = test_well_placed()
 
     purge_cond1(correct_number, correct_index, permutations)
-    print('Number of possible permutations after purging cond1:',len(permutations))
-
-    test_wrongly_placed(cond2, permutations, 1)
-    print('Number of possible permutations after purging cond2:',len(permutations))
-
-    test_wrongly_placed(cond3, permutations, 2)
-    print('Number of possible permutations after purging cond3:',len(permutations))
-
-    test_wrongly_placed(cond4, permutations, 0)
-    print('Number of possible permutations after purging cond4:',len(permutations))
-
-    test_wrongly_placed(cond5, permutations, 1)
-    print('Number of possible permutations after purging cond5:',len(permutations))
+    print('Number of possible permutations after applying condition 1:',len(permutations))
+    wrongly_placed_list = [[cond2, 1], [cond3, 2], (cond4, 0), (cond5, 1)]
+    i = 2
+    for cond, quantity in wrongly_placed_list:
+        test_wrongly_placed(permutations, cond, quantity)
+        print(f'Number of possible permutations after applying condition {i}:',len(permutations))
+        i += 1
 
     for password in permutations:
-        print('The password is',password)
+        print('-' * len(str(f'The password is: {password}')))
+        print('The password is', password)
+        print('-' * len(str(f'The password is: {password}')))
 
 
 def test_well_placed():
@@ -44,7 +40,7 @@ def purge_cond1(correct_number, correct_index, permutations):
         if permu[correct_index] != correct_number:
             permutations.remove(permu)
         
-def test_wrongly_placed(cond, permutations, quantity):
+def test_wrongly_placed(permutations, cond, quantity):
     permu_copy = permutations[:]
     for permu in permu_copy:
         if len(set(cond) & set(permu)) != quantity:
